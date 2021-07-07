a61e8cebb9f4dd885727edc0e05a69f4d73dbc68
DaivaPepalla <73628787+DaivaPepalla@users.noreply.github.com>
2021-07-07 17:19:32 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -132,7 +132,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cosmopolitan.sdp232@gmail.com'
-EMAIL_HOST_PASSWORD = '#Daiva@77783'
+EMAIL_HOST_PASSWORD = ''
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
